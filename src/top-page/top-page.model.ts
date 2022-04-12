932f54cb96116fce8f63e5820955ef9c2fb9bdd4
Viktoria Udodava <viktoria.udodava@gmail.com>
2022-04-12 23:17:00 +0300

    Add auth, product, review, top-page typegoose models

--- src/top-page/top-page.model.ts
+++ src/top-page/top-page.model.ts
@@ -1,27 +1,64 @@
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+import { prop } from '@typegoose/typegoose';
+
 export enum TopLevelCategory {
-    Courses,
-    Services,
-    Books,
-    Products
+	Courses,
+	Services,
+	Books,
+	Products
 }
 
-export class TopPageModel {
-    _id: string;
-    firstCategory: TopLevelCategory;
-    secondCategory: string;
-    title: string;
-    category: string;
-    hh?: {
-        count: number;
-        juniorSalary: number;
-        middleSalary: number;
-        seniorSalary: number;
-    };
-    advantages: {
-        title: string;
-        description: string;
-    }[];
-    seoText: string;
-    textTitle: string;
-    tags: string[];
+class HhData {
+	@prop()
+	count: number;
+
+	@prop()
+	juniorSalary: number;
+
+	@prop()
+	middleSalary: number;
+
+	@prop()
+	seniorSalary: number;
+}
+
+class TopPageAdvantage {
+	@prop()
+	title: string;
+
+	@prop()
+	description: string;
+}
+
+export interface TopPageModel extends Base { }
+export class TopPageModel extends TimeStamps {
+	@prop({ enum: TopLevelCategory })
+	firstCategory: TopLevelCategory;
+
+	@prop()
+	secondCategory: string;
+
+	@prop({ unique: true })
+  alias: string;
+
+	@prop()
+	title: string;
+
+	@prop()
+	category: string;
+
+	@prop({ type: () => HhData })
+	hh?: HhData;
+
+	@prop({ type: () => [TopPageAdvantage] })
+	advantages: TopPageAdvantage[];
+
+	@prop()
+	seoText: string;
+
+	@prop()
+	textTitle: string;
+
+	@prop({ type: () => [String] })
+	tags: string[];
 }
