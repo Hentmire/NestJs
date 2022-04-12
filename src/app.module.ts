5202b6ceb988f0bffd560f4800528bc48adb826c
Viktoria Udodava <viktoria.udodava@gmail.com>
2022-04-12 21:03:42 +0300

    Add docker-compose.yml to configure mongo, connect app to mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,27 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { getMongoConfig } from './configs/mongo.config';
 
 @Module({
 	imports: [
-        ConfigModule.forRoot(),
-        AuthModule,
-        TopPageModule,
-        ProductModule,
-        ReviewModule,
-    ],
+		ConfigModule.forRoot(),
+		TypegooseModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getMongoConfig,
+		}),
+		AuthModule,
+		TopPageModule,
+		ProductModule,
+		ReviewModule,
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
